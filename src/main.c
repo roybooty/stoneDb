0e42ca4dd6ff74b067b6500bb57c801fa988acc0
roybooty <safwanidris4@proton.me>
2026-03-03 23:14:22 +0100

    Implemented the REPL

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   char* buffer;
@@ -16,9 +17,27 @@ InputBuffer* new_input_buffer(){
   return input_buffer;
 }
 
+void print_prompt() {printf("stoneDb> "); }
+
+void read_input(InputBuffer* input_buffer){
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  if(bytes_read <= 0){
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer){
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
 int main(int argc, char* argv[]){
   InputBuffer* input_buffer = new_input_buffer();
-  while(true){
+  while(1){
     print_prompt();
     read_input(input_buffer);
 
